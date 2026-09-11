252bfbb31928764c8b88b8f91879dec90065e926
zamruth <basminazamruth@gmail.com>
2026-09-11 18:41:04 +0530

    core: client config updated

--- apps/client/src/app/app.spec.ts
+++ apps/client/src/app/app.spec.ts
@@ -1,20 +1,27 @@
 import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+
 import { App } from './app';
-import { NxWelcome } from './nx-welcome';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App, NxWelcome],
+      imports: [App],
+      providers: [provideRouter([])],
     }).compileComponents();
   });
 
-  it('should render title', async () => {
+  it('should create the application', () => {
     const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
+
+    expect(fixture.componentInstance).toBeTruthy();
+  });
+
+  it('should render the router outlet', () => {
+    const fixture = TestBed.createComponent(App);
+
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome client',
-    );
+
+    expect(compiled.querySelector('router-outlet')).not.toBeNull();
   });
 });
